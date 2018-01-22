6c761aa887802b5853bac21ca6cff083dd4b01b8
alex-www-marketing <alex.www.marketing@gmail.com>
2018-01-22 16:09:28 +0300

    change oauth

--- R/yadirGetToken.R
+++ R/yadirGetToken.R
@@ -1,6 +1,6 @@
 yadirGetToken <-
 function(){
-  browseURL("https://oauth.yandex.ru/authorize?response_type=token&client_id=365a2d0a675c462d90ac145d4f5948cc")
+  browseURL("https://oauth.yandex.ru/authorize?response_type=token&client_id=c441fa0194f04f4ea2c238c0c2c40ec9")
   token <- readline(prompt = "Enter your token: ")
   return(token)
 }
